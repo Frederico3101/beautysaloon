d9a473f8f17160a70e9edf611f842621634029ee
Fredão <79368020+Frederico3101@users.noreply.github.com>
2021-06-25 18:09:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+// caracteres - string -> são colocados entre aspas
+// number
+// boolean: verifica se é true ou false
+// Tudo que está dentro de um objeto {} é executado corforme os pedidos
+
+/*
+function liquidificador (frutas) {
+  alert(frutas)
+}
+
+liquidificador ('banana') 
+*/
+
+// Variáveis: const (não mutável); var; let (it change  )
+//var nome = 'mayk';
+
+// Atribuindo um objeto
+/*
+const pessoa = {
+  name: 'mayk',
+  age: 37,
+  falar: function () {
+    alert(pessoa.name)
+  }
+}
+
+pessoa.falar()
+*/
+
+// DOM (Document Object Model) modelagem do documento HTML para o js
+
+/*
+document.body.style.background = "red"
+*/
+
+/* Abre e fecha o menu quando clicar no ícone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll') // scroll é maior que a altura do header
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+//  ScrollReveal: mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+// Botão voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.OffsetTop
+    const sectionHeight = section.OffsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.add('active')
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+
+  }
+}
+
+// When scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
